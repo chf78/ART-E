42439c41022e01a5d45584fec98883fedde1e2d9
chf78 <104372326+chf78@users.noreply.github.com>
2023-04-11 23:01:33 -0400

    Update ARTE_ThirdSteps.ino

--- ART-E Testing Software/ARTE_ThirdSteps.ino
+++ ART-E Testing Software/ARTE_ThirdSteps.ino
@@ -1,171 +1,166 @@
-#include <MeMegaPi.h>
-#include <Wire.h>
-#include <SoftwareSerial.h>
 
-MeEncoderOnBoard encoders[3];
+#include <MeMegaPi.h>
+
+/* driving directions */
+#define FORWARD   1
+#define BACKWARD  2
+#define LEFT      3
+#define RIGHT     4
+
+/* state names for autonomous driving */
+#define CLEAR       0
+#define OBSTACLE    1
+#define CHECK_LEFT  2
+#define CHECK_RIGHT 3
+#define GO_LEFT     4
+#define GO_RIGHT    5
+#define TURN_AROUND 6
+
+uint8_t pathState;
+volatile uint8_t leftDist, rightDist;
+
+/* encoder motors on slots 1 and 2 */
+MeEncoderOnBoard Encoder_1(SLOT_1);
+MeEncoderOnBoard Encoder_2(SLOT_2);
 
 MeUltrasonicSensor ultraSensor(PORT_7);
 
-void isr_process_encoder1(void)
-{
-  if(digitalRead(encoders[0].getPortB()) == 0)
-  {
-    encoders[0].pulsePosMinus();
-  }
-  else
-  {
-    encoders[0].pulsePosPlus();;
+/******** interrupt handlers for encoder motors *********/
+void isr_process_encoder1(){
+  if(digitalRead(Encoder_1.getPortB()) == 0){
+    Encoder_1.pulsePosMinus();
+  }else{
+    Encoder_1.pulsePosPlus();
   }
 }
 
-void isr_process_encoder2(void)
-{
-  if(digitalRead(encoders[1].getPortB()) == 0)
-  {
-    encoders[1].pulsePosMinus();
-  }
-  else
-  {
-    encoders[1].pulsePosPlus();
+void isr_process_encoder2(){
+  if(digitalRead(Encoder_2.getPortB()) == 0){
+    Encoder_2.pulsePosMinus();
+  }else{
+    Encoder_2.pulsePosPlus();
   }
 }
+/********************************************************/
 
-void isr_process_encoder3(void)
-{
-  if(digitalRead(encoders[2].getPortB()) == 0)
-  {
-    encoders[2].pulsePosMinus();
-  }
-  else
-  {
-    encoders[2].pulsePosPlus();
-  }
-}
+/******* pulse width modulation setup ************/
+void TimerPWM_init() {
 
-void PWM_init() {
-
-  //Set Pwm 970Hz
+  //Set PWM 8KHz
   TCCR1A = _BV(WGM10);
-  TCCR1B = _BV(CS11) | _BV(CS10) | _BV(WGM12);
+  TCCR1B = _BV(CS11) | _BV(WGM12);
+
   TCCR2A = _BV(WGM21) | _BV(WGM20);
-  TCCR2B = _BV(CS22);
-  TCCR3A = _BV(WGM30);
-  TCCR3B = _BV(CS31) | _BV(CS30) | _BV(WGM32);
+  TCCR2B = _BV(CS21);
   
 }
+/**************************************************/
 
-/**************** Encoder Motor Functions ************************/
-void Forward(int moveSpeed){
-  
-  encoders[0].setMotorPwm(moveSpeed);
-  encoders[1].setMotorPwm(-moveSpeed);
-  
-}
+void setup() {
 
-void Backward(int moveSpeed){
+  // encoder motor setup
+  attachInterrupt(Encoder_1.getIntNum(), isr_process_encoder1, RISING);
+  attachInterrupt(Encoder_2.getIntNum(), isr_process_encoder2, RISING);
   
-  encoders[0].setMotorPwm(-moveSpeed);
-  encoders[1].setMotorPwm(moveSpeed);
+  TimerPWM_init();
   
-}
-
-void TurnLeft(int moveSpeed){
-  
-  encoders[0].setMotorPwm(moveSpeed);
-  encoders[1].setMotorPwm(-moveSpeed/2);
-  
-}
-
-void TurnRight(int moveSpeed){
-  
-  encoders[0].setMotorPwm(moveSpeed/2);
-  encoders[1].setMotorPwm(-moveSpeed);
-  
-}
-
-void Stop(void){
-  
-  encoders[0].setMotorPwm(0);
-  encoders[1].setMotorPwm(0);
-  
-}
-/****************************************************/
-
-void checkLeftRight(void){
-
-  int leftDist, rightDist;
-
-  // look left, then look right
-  TurnLeft(25);
-  delay(500);
-  leftDist = ultraSensor.distanceCm();
-  TurnRight(25);
-  delay(1000);
-  rightDist = ultraSensor.distanceCm();
-  TurnLeft(25);
-  delay(500);
-
-  if((leftDist < 40) && (rightDist < 40)){
-    Backward(50);
-    delay(3000);
-    TurnLeft(50);
-    delay(2000);
-  }
-  if(leftDist < rightDist){
-    TurnRight(50);
-    delay(1000);
-  }
-  if(rightDist < leftDist){
-    TurnLeft(50);
-    delay(1000);
-  }
-}
-
-void setup()
-{
-
-  for(int i=0;i<3;i++){
-    encoders[i].reset(i+1);
-  }
-  
-  attachInterrupt(encoders[0].getIntNum(), isr_process_encoder1, RISING);
-  attachInterrupt(encoders[1].getIntNum(), isr_process_encoder2, RISING);
-  attachInterrupt(encoders[2].getIntNum(), isr_process_encoder3, RISING);
   Serial.begin(115200);
 
-  PWM_init();
+  pathState = CLEAR;  // set the starting state
+  
+}
 
-  for(uint8_t i = 0; i < 3; i++){
-    
-      encoders[i].setPulse(7);
-      encoders[i].setRatio(26.9);
-      encoders[i].setPosPid(1.8,0,1.2);
-      encoders[i].setSpeedPid(0.18,0,0);
-    
+/******** driving functions *************/
+void move(int direction, int speed){
+  int leftSpeed = 0;
+  int rightSpeed = 0;
+  speed = (speed/100.0)*255;
+  if(direction == FORWARD){
+    leftSpeed = -1*speed;
+    rightSpeed = speed;
+  }else if(direction == BACKWARD){
+    leftSpeed = speed;
+    rightSpeed = -1*speed;
+  }else if(direction == LEFT){
+    leftSpeed = speed;
+    rightSpeed = speed;
+  }else if(direction == RIGHT){
+    leftSpeed = -1*speed;
+    rightSpeed = -1*speed;
   }
+  Encoder_1.setMotorPwm(rightSpeed);
+  Encoder_2.setMotorPwm(leftSpeed);
+}
 
+void Stop(){
+  Encoder_1.setMotorPwm(0);
+  Encoder_2.setMotorPwm(0);
+}
+
+void checkLeft(){
+  move(LEFT, 50);
+  delay(1000);
+  Stop();
+  delay(50);
+  leftDist = ultraSensor.distanceCm();
+}
+
+void checkRight(){
+  move(RIGHT, 50);
+  delay(2100);
+  Stop();
+  delay(50);
+  rightDist = ultraSensor.distanceCm();
+}
+
+void turnAround(){
+  move(RIGHT, 50);
+  delay(900);
+  Stop();
+  delay(2500);
+}
+/*********************************************/
+
+/********** finite state machine *************/
+void autoDrive(){
+  int minDistance = 30; // minimum distance in cm for detecting obstacles
+  switch(pathState){
+    case CLEAR:
+      move(FORWARD, 50);
+      if(ultraSensor.distanceCm() <= minDistance){pathState = OBSTACLE;}
+    break;
+    case OBSTACLE:
+      Stop();
+      delay(250);
+      move(BACKWARD, 50);
+      delay(250);
+      Stop();
+      pathState = CHECK_LEFT;
+    break;
+    case CHECK_LEFT:
+      checkLeft();
+      if(leftDist <= 20){pathState = CHECK_RIGHT;}
+      if(leftDist > 20){pathState = CLEAR;}
+    break;
+    case CHECK_RIGHT:
+      checkRight();
+      if(rightDist <= 20){pathState = TURN_AROUND;}
+      if(rightDist > 20){pathState = CLEAR;}
+    break;
+
+    case TURN_AROUND:
+      turnAround();
+      pathState = CLEAR;
+    break;
+  }
+  
 }
 
 void loop() {
 
-  for(int i=0;i<4;i++){
-    encoders[i].loop();
-  }
+  autoDrive();
+  
+  Encoder_1.loop();
+  Encoder_2.loop();
 
-  if(ultraSensor.distanceCm() < 40){
-
-    Stop();
-    delay(1000);
-    encoders[2].moveTo(1500,300);   // lower arm
-    delay(3000);
-    encoders[2].moveTo(0,300);  // raise arm
-    delay(500);
-    checkLeftRight();
-    
-  }
-  else{
-
-    Forward(50);
-    
-  }
 }
